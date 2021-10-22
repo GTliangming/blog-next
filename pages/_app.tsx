72815c26523b2a846799267112bb99cb7262d899
liangming1 <liangming1@lightinthebox.com>
2021-10-22 17:01:04 +0800

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -81,7 +81,7 @@ export default class MyApp extends App<AppProps> {
             name="viewport"
             content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
           />
-          <title>hahahahahahhaahha</title>
+          <title>哈哈哈</title>
         </Head>
         <Layout path={path} query={query} userInfo={customerInfo} isShowNav={SHOW_NAV} language={language}>
           <Component {...pageProps} localeObj={localeObj} />
